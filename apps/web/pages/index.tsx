f275bafe4ca016cf011160f8195c5c1075ead1b8
Manavendra Sen <manavendra4288@gmail.com>
2022-10-15 01:41:17 +0530

    feat: add project screen

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,7 +1,57 @@
-export default function Web() {
+import Button from "components/Button/Button";
+import DashboardLayout from "components/DashboardLayout/DashboardLayout";
+import NextHead from "components/NextHead/NextHead";
+import PageHeader from "components/PageHeader/PageHeader";
+import ProjectTile from "components/ProjectTile/ProjectTile";
+import AddProjectForm from "features/AddProjectForm/AddProjectForm";
+import { FiPlus } from "react-icons/fi";
+import useModal from "store/modalStore";
+
+const projects = [
+  {
+    id: "1",
+    name: "Poptalk",
+    description: "Eu mollit id sit cupidatat aliqua.",
+  },
+  {
+    id: "2",
+    name: "Vinyl",
+    description: "Eu mollit id sit cupidatat aliqua.",
+  },
+];
+
+const Web = () => {
+  const { showModal } = useModal();
+  const createNewProjectHandler = () => {
+    showModal(<AddProjectForm />);
+  };
   return (
-    <div>
-      <h1>Web</h1>
-    </div>
+    <>
+      <NextHead />
+      <DashboardLayout>
+        <PageHeader title='Projects'>
+          <Button
+            onClick={() => {
+              createNewProjectHandler();
+            }}
+          >
+            <FiPlus /> Create New Project
+          </Button>
+        </PageHeader>
+        <main className='mt-6 grid md:grid-cols-3 gap-4 md:gap-6 grid-cols-1'>
+          {projects &&
+            projects.map((project) => (
+              <ProjectTile
+                key={project.id}
+                title={project.name}
+                description={project.description}
+                id={project.id}
+              />
+            ))}
+        </main>
+      </DashboardLayout>
+    </>
   );
-}
+};
+
+export default Web;
