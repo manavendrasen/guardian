6f4ff82971a09a9a751d979d97176f40554e02fe
Manavendra Sen <manavendra4288@gmail.com>
2022-10-15 04:10:25 +0530

    feat: add schema types

--- apps/web/types/Config.ts
+++ apps/web/types/Config.ts
@@ -1,4 +1,16 @@
+import { Environment } from "constants/Environments";
+import { ConfigTeam } from "./ConfigTeam";
+import { Project } from "./Project";
+import { Secret } from "./Secret";
+
 export type Config = {
   id: string;
-  environment: string;
+  environment: Environment;
+  name: string;
+  projectId: string;
+  project: Project;
+  secrets: Secret[];
+  configMember: ConfigTeam[];
+  createdAt: Date;
+  updatedAt: Date;
 };
