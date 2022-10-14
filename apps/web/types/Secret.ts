b56b57af34c82a01fa952b034380ebca80cbb3ab
Manavendra Sen <manavendra4288@gmail.com>
2022-10-15 04:10:25 +0530

    feat: add schema types

--- apps/web/types/Secret.ts
+++ apps/web/types/Secret.ts
@@ -1,1 +1,12 @@
-export type Secret = {};
+import { Config } from "./Config";
+
+export type Secret = {
+  id: string;
+  name: string;
+  value: string;
+  comment: string;
+  configId: string;
+  config: Config;
+  createdAt: Date;
+  updatedAt: Date;
+};
