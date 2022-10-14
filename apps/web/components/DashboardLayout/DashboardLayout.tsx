36b528597d877fc0d8f930fbf55d12d703a1bab6
Manavendra Sen <manavendra4288@gmail.com>
2022-10-15 05:10:17 +0530

    feat: add project page

--- apps/web/components/DashboardLayout/DashboardLayout.tsx
+++ apps/web/components/DashboardLayout/DashboardLayout.tsx
@@ -8,11 +8,11 @@ interface DashboardLayoutProps {
 
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
   return (
-    <div className='flex h-screen overflow-hidden'>
+    <div className='flex overflow-hidden'>
       <Sidebar />
       <div className='relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden'>
         <Header />
-        <main className='px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto'>
+        <main className='px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto h-5/6'>
           {children}
         </main>
       </div>
--- apps/web/components/ProjectTile/ProjectTile.tsx
+++ apps/web/components/ProjectTile/ProjectTile.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 interface ProjectTileProps {
@@ -12,16 +12,19 @@ const ProjectTile: React.FC<ProjectTileProps> = ({
   title,
   description,
 }) => {
+  const router = useRouter();
+  const routeToProjectPage = () => {
+    // fetch product details
+    router.push(`/project/${id}`);
+  };
   return (
-    <Link href={`/project/${id}`}>
-      <div
-        className='bg-slate-100 flex flex-col gap-2 p-6 rounded-md hover:border-primary focus:border-2 hover:border-2 duration-100'
-        onClick={() => {}}
-      >
-        <h5 className='text-lg font-medium'>{title}</h5>
-        <p className='text-slate-500'>{description}</p>
-      </div>
-    </Link>
+    <div
+      className='bg-slate-100 flex flex-col gap-2 p-6 rounded-md hover:border-primary focus:border-2 hover:border-2 duration-100'
+      onClick={routeToProjectPage}
+    >
+      <h5 className='text-lg font-medium'>{title}</h5>
+      <p className='text-slate-500'>{description}</p>
+    </div>
   );
 };
 
