32450251134fc496ab5bdd3ee0ee9c149c0c002b
Manavendra Sen <manavendra4288@gmail.com>
2022-10-15 17:39:39 +0530

    feat: add member form

--- apps/web/store/projectStore.tsx
+++ apps/web/store/projectStore.tsx
@@ -1,3 +1,4 @@
+import { addMemberToProject } from "requests/projectRequests";
 import { Project } from "types/Project";
 import create from "zustand";
 
@@ -21,13 +22,17 @@ const sampleProjects = [
 ];
 
 type TProject = {
+  loading: boolean;
+  setLoading: (loading: boolean) => void;
   project: Project | null;
   setProject: (project: Project) => void;
   projects: Project[];
   setProjects: (projects: Project[]) => void;
+  addMembersToProject: (members: string[]) => void;
 };
 
-const useProjectStore = create<TProject>((set) => ({
+const useProjectStore = create<TProject>((set, get) => ({
+  loading: false,
   project: sampleProject,
   projects: sampleProjects,
   setProject: (project: Project) => {
@@ -35,6 +40,31 @@ const useProjectStore = create<TProject>((set) => ({
   },
   setProjects: (projects: Project[]) => {
     set((state: TProject) => ({ ...state, projects: projects }));
+  },
+  setLoading: (loading) => {
+    set((state: TProject) => ({ ...state, loading }));
+  },
+  addMembersToProject: async (members: string[]) => {
+    get().setLoading(true);
+
+    const project = get().project;
+    if (project) {
+      const { id } = project;
+
+      // TODO: add encrypted project key
+      const membersWithEncKey = members.map((member, index) => {
+        return {
+          email: member,
+          encProjectKey: index,
+        };
+      });
+
+      const res = await addMemberToProject(id, membersWithEncKey);
+      // return res.statusCode === 200;
+    } else {
+      get().setLoading(false);
+      // return false;
+    }
   },
 }));
 
