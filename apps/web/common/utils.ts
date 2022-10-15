32d05f9790777f97ec82b0bbbd9d2d3f10bbacb1
Manavendra Sen <manavendra4288@gmail.com>
2022-10-16 04:16:57 +0530

    fix: auth

--- apps/web/common/utils.ts
+++ apps/web/common/utils.ts
@@ -1,28 +1,30 @@
 export class Utils {
   static fromStringToBuffer(data: string): ArrayBuffer {
-    // const arrayBuf = new Uint8Array(data.length);
+    const arrayBuf = new Uint8Array(data.length);
 
-    // for (let i = 0; i < data.length; i++) {
-    //   arrayBuf[i] = data.charCodeAt(i);
-    // }
+    for (let i = 0; i < data.length; i++) {
+      arrayBuf[i] = data.charCodeAt(i);
+    }
 
-    // return arrayBuf;
+    return arrayBuf;
 
-    const enc = new TextEncoder();
-    return enc.encode(data);
+    // const enc = new TextEncoder();
+    // return enc.encode(data);
   }
 
   static fromBufferToString(data: ArrayBuffer): string {
-    // const arrayBuf = new Uint8Array(data);
-    // let res = "";
+    const arrayBuf = new Uint8Array(data);
+    let res = "";
 
-    // for (let i = 0; i < arrayBuf.byteLength; i++) {
-    //   res += String.fromCharCode(arrayBuf[i]);
-    // }
+    for (let i = 0; i < arrayBuf.byteLength; i++) {
+      res += String.fromCharCode(arrayBuf[i]);
+    }
 
-    const dec = new TextDecoder();
+    return res;
 
-    return dec.decode(data);
+    // const dec = new TextDecoder();
+
+    // return dec.decode(data);
   }
 
   static fromB64ToBuffer(data: string): ArrayBuffer {
